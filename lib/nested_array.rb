5799ed3f7b63f0bbe18d57a9898e33435b46f744
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-12 21:12:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -46,7 +46,7 @@ def matrix_lookup(matrix, row, column)
   #CONVENTIONAL_PRODUCE
   [["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"],
   #ORGANIC_PRODUCE
-  ["Strawberries", "Potatoes", "Grapes", "Avocadoes","Asparagus"][]]
+  ["Strawberries", "Potatoes", "Grapes", "Avocadoes","Asparagus"]]
   
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
